5497d5fe5b1018e7257724ede4a8997cd305a66a
HarikrishnaJaka <jakahari016@gmail.com>
2025-12-04 14:25:35 +0530

    intial commit

---
+++ HariKrishna-1/src/main/java/com/hk/HariKrishna1Application.java
@@ -0,0 +1,13 @@
+package com.hk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HariKrishna1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HariKrishna1Application.class, args);
+	}
+
+}
---
+++ HariKrishna-1/src/main/java/com/hk/hari/Harikrishna.java
@@ -0,0 +1,16 @@
+package com.hk.hari;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hari")
+public class Harikrishna 
+{
+	@GetMapping("/wish")
+	public String wish() {
+		return "hello boss how are you";
+	}
+
+}
